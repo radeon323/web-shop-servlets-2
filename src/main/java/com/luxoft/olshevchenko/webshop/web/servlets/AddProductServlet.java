05ddfa21227d9e7f3f57a1e864b905c97fb6a3a7
Oleksandr Shevchenko <4336399@gmail.com>
2022-02-23 19:54:48 +0200

    AddProductServlet and EditProductServlet refactoring

--- src/main/java/com/luxoft/olshevchenko/webshop/web/servlets/AddProductServlet.java
+++ src/main/java/com/luxoft/olshevchenko/webshop/web/servlets/AddProductServlet.java
@@ -3,6 +3,7 @@ package com.luxoft.olshevchenko.webshop.web.servlets;
 import com.luxoft.olshevchenko.webshop.entity.Product;
 import com.luxoft.olshevchenko.webshop.service.ProductService;
 import com.luxoft.olshevchenko.webshop.web.templater.PageGenerator;
+import lombok.SneakyThrows;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Oleksandr Shevchenko
@@ -28,39 +30,47 @@ public class AddProductServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         try {
-            String name = request.getParameter("name");
-            if(name != null && name.length() > 0 && request.getParameter("price") != null) {
-                Product product = getProduct(request);
-                productService.add(product);
-
-                String msgSuccess = "Product <i>" + name + "</i> was successfully added!";
-                Map<String, Object> parameters = Map.of("msgSuccess", msgSuccess);
-                String page = PageGenerator.getPage("add_product.html", parameters);
-                response.getWriter().write(page);
-
-            } else {
-                String errorMsg = "Please fill up all fields!";
-                Map<String, Object> parameters = Map.of("errorMsg", errorMsg);
-                String pageError = PageGenerator.getPage("add_product.html", parameters);
-                response.getWriter().write(pageError);
-            }
+            Optional<Product> optionalProduct = validateAndGetProduct(request, response);
+            optionalProduct.ifPresent(product -> addProduct(product, response));
         } catch (Exception e) {
-            String errorMsg = "Please fill up all fields!";
-            Map<String, Object> parameters = Map.of("errorMsg", errorMsg);
-            String pageError = PageGenerator.getPage("add_product.html", parameters);
-            response.getWriter().write(pageError);
+            writeErrorResponse(response);
         }
     }
 
-    private Product getProduct(HttpServletRequest request) {
-        Product product = Product.builder()
-                .name(request.getParameter("name"))
-                .price(Double.parseDouble(request.getParameter("price")))
-                .creationDate(LocalDateTime.now().withNano(0).withSecond(0))
-                .build();
-        return product;
+    @SneakyThrows
+    private void addProduct(Product product, HttpServletResponse response) {
+        productService.add(product);
+        String msgSuccess = "Product <i>" + product.getName() + "</i> was successfully added!";
+        Map<String, Object> parameters = Map.of("msgSuccess", msgSuccess);
+        String page = PageGenerator.getPage("add_product.html", parameters);
+        response.getWriter().write(page);
+    }
+
+    @SneakyThrows
+    private Optional<Product> validateAndGetProduct(HttpServletRequest request, HttpServletResponse response) {
+        String name = request.getParameter("name");
+        String price = request.getParameter("price");
+        if(name != null && name.length() > 0 && price != null && Double.parseDouble(price) > 0 ) {
+            Product product = Product.builder()
+                    .name(name)
+                    .price(Double.parseDouble(price))
+                    .creationDate(LocalDateTime.now().withNano(0).withSecond(0))
+                    .build();
+            return Optional.of(product);
+        } else {
+            writeErrorResponse(response);
+            return Optional.empty();
+        }
+    }
+
+    @SneakyThrows
+    private void writeErrorResponse(HttpServletResponse response) {
+        String errorMsg = "Please fill up all fields!";
+        Map<String, Object> parameters = Map.of("errorMsg", errorMsg);
+        String pageError = PageGenerator.getPage("add_product.html", parameters);
+        response.getWriter().write(pageError);
     }
 
 
--- src/main/java/com/luxoft/olshevchenko/webshop/web/servlets/EditProductServlet.java
+++ src/main/java/com/luxoft/olshevchenko/webshop/web/servlets/EditProductServlet.java
@@ -3,18 +3,21 @@ package com.luxoft.olshevchenko.webshop.web.servlets;
 import com.luxoft.olshevchenko.webshop.entity.Product;
 import com.luxoft.olshevchenko.webshop.service.ProductService;
 import com.luxoft.olshevchenko.webshop.web.templater.PageGenerator;
+import lombok.SneakyThrows;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Oleksandr Shevchenko
  */
 public class EditProductServlet extends HttpServlet {
-    private ProductService productService;
+    private final ProductService productService;
 
     public EditProductServlet(ProductService productService) {
         this.productService = productService;
@@ -33,40 +36,50 @@ public class EditProductServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HashMap<String, Object> parameters = new HashMap<>();
         try {
-            int id = Integer.parseInt(request.getParameter("id"));
-            Product product = getProduct(request);
-            product.setId(id);
-            HashMap<String, Object> parameters = new HashMap<>();
-            parameters.put("product", product);
-
-            String name = request.getParameter("name");
-            if(name != null && name.length() > 0 && request.getParameter("price") != null) {
-                productService.edit(product);
-
-                String msgSuccess = "Product <i>" + name + "</i> was successfully changed!";
-                parameters.put("msgSuccess", msgSuccess);
-                String page = PageGenerator.getPage("edit_product.html", parameters);
-                response.getWriter().write(page);
-
-            } else {
-                String errorMsg = "Please fill up all fields";
-                parameters.put("errorMsg", errorMsg);
-                String page = PageGenerator.getPage("edit_product.html", parameters);
-                response.getWriter().write(page);
-            }
+            Optional<Product> optionalProduct = validateAndGetProduct(request, response, parameters);
+            optionalProduct.ifPresent(product -> editProduct(product, response, parameters));
         } catch (Exception e) {
             String errorMsg = "Please fill up all fields";
             response.getWriter().println(errorMsg);
         }
     }
 
-    private Product getProduct(HttpServletRequest request) {
-        Product product = Product.builder()
-                .name(request.getParameter("name"))
-                .price(Double.parseDouble(request.getParameter("price")))
-                .build();
-        return product;
+    @SneakyThrows
+    private void editProduct(Product product, HttpServletResponse response, HashMap<String, Object> parameters) {
+        productService.edit(product);
+        String msgSuccess = "Product <i>" + product.getName() + "</i> was successfully changed!";
+        parameters.put("msgSuccess", msgSuccess);
+        String page = PageGenerator.getPage("edit_product.html", parameters);
+        response.getWriter().write(page);
+    }
+
+    @SneakyThrows
+    private Optional<Product> validateAndGetProduct(HttpServletRequest request, HttpServletResponse response, HashMap<String, Object> parameters) {
+        String name = request.getParameter("name");
+        String price = request.getParameter("price");
+        int id = Integer.parseInt(request.getParameter("id"));
+        if(name != null && name.length() > 0 && price != null && Double.parseDouble(price) > 0 ) {
+            Product product = Product.builder()
+                    .id(id)
+                    .name(name)
+                    .price(Double.parseDouble(price))
+                    .build();
+            parameters.put("product", product);
+            return Optional.of(product);
+        } else {
+            writeErrorResponse(response);
+            return Optional.empty();
+        }
+    }
+
+    @SneakyThrows
+    private void writeErrorResponse(HttpServletResponse response) {
+        String errorMsg = "Please fill up all fields!";
+        Map<String, Object> parameters = Map.of("errorMsg", errorMsg);
+        String page = PageGenerator.getPage("edit_product.html", parameters);
+        response.getWriter().write(page);
     }
 
 

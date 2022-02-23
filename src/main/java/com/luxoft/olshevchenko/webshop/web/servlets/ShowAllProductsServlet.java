7569e87b1660180d261dc27dbc23012fd8e98617
Oleksandr Shevchenko <4336399@gmail.com>
2022-02-23 19:56:19 +0200

    ShowAllProductsServlet refactoring

--- src/main/java/com/luxoft/olshevchenko/webshop/web/servlets/ShowAllProductsServlet.java
+++ src/main/java/com/luxoft/olshevchenko/webshop/web/servlets/ShowAllProductsServlet.java
@@ -2,11 +2,13 @@ package com.luxoft.olshevchenko.webshop.web.servlets;
 
 import com.luxoft.olshevchenko.webshop.entity.Product;
 import com.luxoft.olshevchenko.webshop.service.ProductService;
+import com.luxoft.olshevchenko.webshop.service.SecurityService;
 import com.luxoft.olshevchenko.webshop.web.templater.PageGenerator;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -16,34 +18,32 @@ import java.util.List;
  */
 public class ShowAllProductsServlet extends HttpServlet {
     private final ProductService productService;
+    private final SecurityService securityService;
 
-    public ShowAllProductsServlet(ProductService productService) {
+    public ShowAllProductsServlet(ProductService productService, SecurityService securityService) {
         this.productService = productService;
+        this.securityService = securityService;
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        List<Product> products = productService.findAll();
         HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("products", products);
-
+        dataForProductsList(request, parameters);
         String page = PageGenerator.getPage("products_list.html", parameters);
         response.getWriter().write(page);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        List<Product> products = productService.findAll();
         HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("products", products);
+        dataForProductsList(request, parameters);
 
         try {
             int id = Integer.parseInt(request.getParameter("id"));
-            Product product = productService.findById(id);
 
             productService.remove(id);
 
-            String msgSuccess = "Product " + product.getName() + " was successfully deleted!";
+            String msgSuccess = "Product " + id + " was successfully deleted!";
             parameters.put("msgSuccess", msgSuccess);
 
             String page = PageGenerator.getPage("products_list.html", parameters);
@@ -52,6 +52,17 @@ public class ShowAllProductsServlet extends HttpServlet {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void dataForProductsList(HttpServletRequest request, HashMap<String, Object> parameters) {
+        List<Product> products = productService.findAll();
+        HttpSession session = request.getSession();
+        String email = (String) session.getAttribute("email");
+        String id = (String) session.getAttribute("userId");
+        parameters.put("products", products);
+        parameters.put("email", email);
+        parameters.put("userId", id);
+        parameters.put("login", Boolean.toString(securityService.isAuth(request)));
     }
 
 
